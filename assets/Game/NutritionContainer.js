338a357ef7fb59ccbbd4390de5606f99e13cbef4
江逸帆 <yifan.jiang@aliensidea.com>
2017-06-18 10:47:45 +0800

    修改洞的生成方式

--- assets/Game/NutritionContainer.js
+++ assets/Game/NutritionContainer.js
@@ -1,26 +1,33 @@
 const Tween = require('TweenLite');
+var HoleType = require("Types").HoleType;
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
         icon:cc.Sprite,
         nutrition:0,
-        showDuration:0
+        showDuration:0,
+        type: {
+            default: HoleType.Turd,
+            type: HoleType
+        }
     },
 
     // use this for initialization
-    init (nutrition, nutritionType, resMng) {
+    init (nutrition, resMng) {
         this.resMng = resMng;
         this.icon.node.opacity = 255;
         if (nutrition) {
             this.nutrition = nutrition;
         }
         this.flashNode = this.node.getChildByName('flash');
-        this.node.active = false;
+        // this.node.active = false;
+        this.show();
     },
 
     show () {
-        this.node.active = true;
+        // this.node.active = true;
         this.flashNode.opacity = 255;
         Tween.to(this.flashNode, this.showDuration, {
             opacity: 0
